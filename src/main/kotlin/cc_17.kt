481f791efd1a38ecae72c07879fdef9af42f0ed5
OlgaBarbu <barbuolga80@gmail.com>
2022-06-06 18:19:16 +0200

    Update on Challenge 17

--- src/main/kotlin/cc_17.kt
+++ src/main/kotlin/cc_17.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 /*
     Coding Challenge #17
     Have the function kaprekars_constant(num) take the num parameter being passed which will be a 4-digit number with at least two distinct digits.
@@ -14,4 +16,57 @@
 
 fun main(args: Array<String>) {
 
+    val s = Scanner(System.`in`)
+
+    print("Type a four digit number: ")
+    val enteredString = s.nextInt()
+
+    kaprekarsConstant(enteredString,0)
+
+} fun kaprekarsConstant(num : Int, c: Int): Int {
+    //Declaring variable to store iteration count, ascending and descending & initializing them to c, 0
+    var count = c
+    var asc = 0
+    var desc = 0
+
+    var newNum = num
+
+    val array = IntArray(4)
+
+    if(  num <= 999 || num >=10000 ){  //Throws exception if the input number is not a four digit lenght
+         throw java.lang.Exception("That's not a four digit number, it should look like this: 1234, 4567, 7890, 0156.")
+    }
+    // Storing array of digits in inverse order.
+   for(i in 0..3) {
+
+        array[i] = newNum % 10
+
+        newNum = newNum /10
+   }
+    // Sorting array, and storing it in ascending order and descending order.
+     array.sort()
+
+    for( j in 0..3) {
+
+        asc = asc * 10 + array[j]
+    }
+    for (x in 3 downTo 0){
+        desc = desc * 10 + array[x]
+    }
+
+    // Returns difference between numbers formed by descending and ascending order.
+    val diff = desc - asc
+
+     if( diff == num) {
+
+        print("And Kaprekar's constant $num has been reached \n")
+        // returning count of iterations when the constant is reached
+        print("The number of times the routine repeated: $count")
+        return diff
+    }
+    println("The difference between the biggest number $desc and smallest ${String.format("%04d",asc)} number is equal to: $diff")
+    //Repeating until value reaches Kaprekar's constant.
+    count++
+
+    return kaprekarsConstant(diff, count)
 }
